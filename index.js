1b97507937e4d8ff6280c818ebcbe0f98d8fe464
Juampi44 <juampiallstar@hotmail.com>
2022-10-02 23:09:07 -0300

    primerentrega

--- index.js
+++ index.js
@@ -1,61 +1,36 @@
-// alert("hola")
-
-
-// function sumar_kit(precio){
-//     let kit = precio + 1000;
-//     return kit;    
-    
-// }
-
-// alert("bienvenido");
-// alert("Todos los kits valen $1000");
-// let precio = parseInt(prompt("ingrese el precio del producto sin kit"));
+/* Primer preentrega */
 
 
 
-// console.log("precio del kit: " , precio);
+ alert("bienvenido");
+    alert("Las marcas son: KTM , KAWASAKI , HONDA , DUCATI , YAMAHA , BMW");
+    alert("En el momento que escriba el nombre de la marca tiene que hacerlo todo en mayusculas");
 
-// let precio_mas_kit = sumar_kit(precio);
-// console.log("el precio total es de: " , precio_mas_kit);
-
-
-
-class Modelo_moto{
-    constructor(nombre , cilindrada, estilo , año , stock, precio){
-        this.nombre = nombre;
-        this.cilindrada = cilindrada;
-        this.estilo = estilo;
-        this.año = año;
-        this.stock = stock;
-        this.precio = precio;
+    let nombre = prompt ("Escriba el nombre de la marca que busca");
+    if (nombre == "KTM"){
+        console.log("Su marca seleccionada es:" , nombre);
+        console.log("Nuestros modelos de la marca seleccionada son: RC, ADVENTURE, DUKE, MOTOCROSS");
     }
-    get_datos(){
-        console.log("<---datos de la moto--->");
-        console.log("Nombre: ", this.nombre);
-        console.log("cilindrada: ", this.cilindrada);
-        console.log("estilo: ", this.estilo);
-        console.log("año: ", this.año);
-        console.log("cantidad de unidades: " , this.stock);
-        console.log("Precio de este modelo 0KM sin IVA: " , this.precio);
+    else if (nombre == "KAWASAKI"){
+        console.log("Su marca seleccionada es:" , nombre);
+        console.log("Nuestros modelos de la marca seleccionada son: Kawasaki Z , ZXRR, NINJA, VERYS")
     }
-    get_stock(){
-        if(this.stock < 10){
-            console.log("No hay stock en este momento, solo hay unidades para test-drive")
-        }
+    else if (nombre == "BMW"){
+        console.log("Su marca seleccionada es:" , nombre);
+        console.log("Nuestros modelos de la marca seleccionada son: RRS, FR, SRR , RGS ADVENTURE")
     }
-    get_iva(){
-        console.log("el precio con iva es de: ", this.precio * 0.21)
+    else if (nombre =="HONDA"){
+        console.log("Su marca seleccionada es:" , nombre);
+        console.log("Nuestros modelos de la marca seleccionada son: CBR, CBR FIREBLADE, AFRICA TWIN, CRFR")
     }
-}
-
-let moto_uno = new Modelo_moto("KTM", 1290, "Naked" , 2022 , 15 , 1700980);
-let moto_dos = new Modelo_moto("Yamaha" , 900 , "Touring" , 2021 , 4 , 950000);
-
-
-
-moto_uno.get_datos();
-moto_uno.get_stock();
-moto_uno.get_iva();
-moto_dos.get_datos();
-moto_dos.get_stock();
-moto_dos.get_iva();
+    else if (nombre == "YAMAHA"){
+        console.log("Su marca seleccionada es:" , nombre);
+        console.log("Nuestros modelos de la marca seleccionada son: MT, YZF, YZ, SUPER TENERE")
+    }
+    else if (nombre == "DUCATI"){
+        console.log("Su marca seleccionada es:" , nombre);
+        console.log("Nuestros modelos de la marca seleccionada son: PANIGALE, MONSTER, STREET FIGHTER, MULTISTRADA")
+    }
+    else{
+        console.log("Escriba el nombre de la marca que busca por favor");
+    }
